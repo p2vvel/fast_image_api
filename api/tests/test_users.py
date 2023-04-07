1fac8e974dc13a2c8469f03b6a1a972c98943aea
p2vvel <p2vvel@gmail.com>
2023-04-07 15:46:17 +0200

    Test utils rebuilt, add fixture cleaning db

--- api/tests/test_users.py
+++ api/tests/test_users.py
@@ -1,18 +1,33 @@
 from fastapi.testclient import TestClient
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from .utils import app, clean_db
+import datetime
 
 
-from .utils import create_test_app
-
-
-app = create_test_app()
 client = TestClient(app)
 
+
+def test_user_create(clean_db):
+    response = client.post(
+        "/auth/users", json={"username": "pawel", "password": "1234"}
+    )
+
+    assert response.status_code == 200
+    json = response.json()
+    assert json.get("username") == "pawel"
+    assert "password" not in json  # there should be no password sent in response
+    assert json.get("is_superuser") == False
+    assert json.get("is_active") == True
+    assert (
+        json.get("id") == 1
+    )  # TODO: is it good idea to test it? might be dependable on db engine
+
+    # creation and update times should be equal at the creation moment,
+    # although there are some minor differences in miliseconds due to default values being taken separately
+    created_at = datetime.datetime.fromisoformat(json.get("created_at"))
+    updated_at = datetime.datetime.fromisoformat(json.get("updated_at"))
+    assert updated_at - created_at < datetime.timedelta(seconds=0.1)
 
 
 def test_users():
     response = client.get("/auth/users")
     assert response.status_code == 401
-
-    
--- api/tests/utils.py
+++ api/tests/utils.py
@@ -1,24 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import pytest
 
-from .. import config
+from ..main import app
 from ..database import Base, get_db
+from .. import config
 
 
+engine = create_engine(config.TEST_DB_URL, connect_args={"check_same_thread": False})
+testing_local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def create_test_app():
-    from ..main import app
-    engine = create_engine(config.TEST_DB_URL, connect_args={"check_same_thread": False})
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    def override_get_db():
-        try:
-            db = TestingSessionLocal()
-            yield db
-        finally:
-            db.close()
+def override_get_db():
+    try:
+        db = testing_local_session()
+        yield db
+    finally:
+        db.close()
 
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture()
+def clean_db():
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    app.dependency_overrides[get_db] = override_get_db
-
-    return app

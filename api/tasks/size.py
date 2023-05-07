1477195abc8036906ad02e6ddd6cc9f108b1a373
p2vvel <p2vvel@gmail.com>
2023-05-07 18:15:48 +0200

    Add typing

--- api/tasks/size.py
+++ api/tasks/size.py
@@ -2,7 +2,7 @@ from PIL import Image
 from ..schemas import transform as schema
 
 
-def change_size(img: Image, size: schema.Size):
+def change_size(img: Image, size: schema.Size) -> Image:
     """Change image size according to size value"""
     if size.x == size.y == 0:
         return img      # don't resize if both dimensions are 0

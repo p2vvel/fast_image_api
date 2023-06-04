d4926ef92f02409e13c946cc4a0454bf22c06ed8
p2vvel <p2vvel@gmail.com>
2023-06-04 22:13:33 +0200

    Return binary images

--- api/models/image.py
+++ api/models/image.py
@@ -1,13 +1,15 @@
 from api.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
-from ..config import IMAGE_URL
-
+from ..config import IMAGE_URL, FILE_STORAGE
+from uuid import uuid4, UUID
+from pathlib import Path
 
 class Image(Base):
     __tablename__ = "image"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[UUID] = mapped_column(unique=True, nullable=False, index=True)
     original_filename: Mapped[str]
     filename: Mapped[str] = mapped_column(unique=True)
 
@@ -18,10 +20,14 @@ class Image(Base):
     original_image: Mapped["Image"] = relationship()  # noqa: F821
 
     @property
-    def path(self) -> str:
+    def url(self) -> str:
         user_uuid = self.user.uuid
-        image_uuid = self.filename.split('.')[0]
+        image_uuid = self.uuid
         return f"{IMAGE_URL}/{user_uuid}/{image_uuid}"
+
+    @property
+    def path(self) -> Path:
+        return Path(FILE_STORAGE) / str(self.user.uuid) / self.filename
 
     def __repr__(self):
         return f"<Image: {self.id}>"

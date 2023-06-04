e7aff840d0af25e581395ebc7df04463c212e02a
p2vvel <p2vvel@gmail.com>
2023-06-05 00:32:52 +0200

    Invoke celery

--- api/routers/images.py
+++ api/routers/images.py
@@ -7,7 +7,8 @@ from ..database import get_db
 from ..cruds import image as image_crud
 from ..models import User
 from ..dependencies.auth import get_user_or_401
-from uuid import UUID
+from uuid import UUID, uuid4
+from api.tasks.images import edit_image
 
 
 router = APIRouter()
@@ -37,19 +38,30 @@ def get_original_image(
     image_uuid: UUID,
     db: Session = Depends(get_db),
     user: User = Depends(get_user_or_401),
+) -> FileResponse:
+    image = image_crud.get_image_by_uuid(image_uuid, db)
+    if user_uuid != user.uuid or image.user != user:
+        raise HTTPException(status_code=403)
+
+    return FileResponse(image.path)
+
+
+@router.get("/{user_uuid}/{image_uuid}/transform")
+def send_image_to_celery(
+    user_uuid: UUID,
+    image_uuid: UUID,
+    transform: schemas.Transform,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_user_or_401),
 ):
     image = image_crud.get_image_by_uuid(image_uuid, db)
     if user_uuid != user.uuid or image.user != user:
         raise HTTPException(status_code=403)
 
-    return FileResponse(image.path, media_type="image/png")
+    new_filename = f"{uuid4()}.png"
+    temp = edit_image.delay(image.path, new_filename, transform)
 
-
-# @router.get("/{user_uuid}/{image_uuid}/transform")
-# def edit_image(
-#     user_uuid: UUID,
-#     image_uuid: UUID,
-#     db: Session = Depends(get_db),
-#     user: User = Depends(get_user_or_401),
-# ):
-#   pass
+    return temp.id
+    # return 
+    # return FileResponse(image.path)
+    return "TODO: implement image editing"

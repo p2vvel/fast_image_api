5e8997a8026fa888868c92dd5c1884fb0684211e
p2vvel <p2vvel@gmail.com>
2023-06-13 22:46:39 +0200

    Use lifespan function

--- api/main.py
+++ api/main.py
@@ -3,28 +3,30 @@ from fastapi.staticfiles import StaticFiles
 import os
 from fastapi import Depends
 from sqlalchemy.orm import Session
-
 from .database import Base, engine, get_db
 from .routers import auth_router, images_router, users_router
 from .config import settings
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # check folder for serving static files
+    if not os.path.exists("./images"):
+        os.mkdir("./images")
+    
+    # mount routers
+    app.mount("/static", StaticFiles(directory="./images"), name="static")
+    app.include_router(auth_router, prefix="/auth")
+    app.include_router(users_router, prefix="/users")
+    app.include_router(images_router, prefix=settings.image_url)
+
+    # make all migrations
+    Base.metadata.create_all(bind=engine)
+    yield
 
 
-# serve static files
-if not os.path.exists("./images"):
-    os.mkdir("./images")
-
-
-app.mount("/static", StaticFiles(directory="./images"), name="static")
-app.include_router(auth_router, prefix="/auth")
-app.include_router(users_router, prefix="/users")
-app.include_router(images_router, prefix=settings.image_url)
-
-
-# make all migrations
-Base.metadata.create_all(bind=engine)
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
